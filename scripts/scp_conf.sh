53a5dd0b0c4bb91068ab5168eb7325c3b97e1d36
simplejoy <lantian-lz@163.com>
2015-04-23 18:40:32 +0800

    favicon更改

--- scripts/scp_conf.sh
+++ scripts/scp_conf.sh
@@ -1,8 +1,8 @@
 
-scp root@www.zhixuan.com:/etc/redis.conf /etc/
+#scp root@www.zhixuan.com:/etc/redis.conf /etc/
 
-scp root@www.eqcj.com:/etc/nginx/nginx.conf /etc/nginx/
-scp root@www.eqcj.com:/etc/nginx/conf.d/* /etc/nginx/conf.d/
+scp root@www.aoaoxc.com:/etc/nginx/nginx.conf /etc/nginx/
+scp root@www.aoaoxc.com:/etc/nginx/conf.d/* /etc/nginx/conf.d/
 
-scp -r root@www.eqcj.com:/etc/supervisord* /etc/
-scp root@www.eqcj.com:/etc/init.d/supervisord /etc/init.d/
+scp -r root@www.aoaoxc.com:/etc/supervisord* /etc/
+scp root@www.aoaoxc.com:/etc/init.d/supervisord /etc/init.d/
